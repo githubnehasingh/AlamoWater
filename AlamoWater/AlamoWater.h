b986819246a1408ec0a04b8ec979127d17e630cb
Neha_headerlabs <neha@headerlabs.com>
2018-03-06 13:10:09 +0530

    Initial Commit

---
+++ AlamoWater/AlamoWater.h
@@ -0,0 +1,19 @@
+//
+//  AlamoWater.h
+//  AlamoWater
+//
+//  Created by Neha on 3/6/18.
+//  Copyright © 2018 Neha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AlamoWater.
+FOUNDATION_EXPORT double AlamoWaterVersionNumber;
+
+//! Project version string for AlamoWater.
+FOUNDATION_EXPORT const unsigned char AlamoWaterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlamoWater/PublicHeader.h>
+
+
